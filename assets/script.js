bfae7b96e41ab67d5d8ecc0fd4473cb5be342bc9
mikevampm323 <mike.vartanyan@yahoo.com>
2020-02-05 22:30:22 -0800

    ajax call

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    })
+
+        .then(function (response) {
+
+            console.log("Hello There");
+        })
+
+
+
+
+
+
+
+
+
+
+})
