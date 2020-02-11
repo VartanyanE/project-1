f81e56d3af427c92d466f29e5f30128866a7019e
mikevampm323 <mike.vartanyan@yahoo.com>
2020-02-10 23:04:33 -0800

    ajax request

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,4 @@
 
-var x;
-var y;
-
 $(document).ready(function () {
     var ApiKey = "490204d27c988ccb9e991f177de168ad";
     var requestURL = "https://developers.zomato.com/api/v2.1/geocode";
@@ -11,42 +8,29 @@ $(document).ready(function () {
     if ("geolocation" in navigator) {
 
         navigator.geolocation.getCurrentPosition(function (position) {
-            // console.log(position);
-            // console.log(position.coords.latitude);
-            // console.log(position.coords.longitude);
+
             x = position.coords.latitude;
             y = position.coords.longitude;
+            ajaxCall(x, y);
         });
     } else {
         console.log("doesn't work")
     }
 
+    function ajaxCall(x, y) {
 
+        $.ajax(requestURL + "?lat=" + x + "&lon=" + y,
+            {
+                type: "GET",
+                success: geoLoc,
+                headers: {
+                    "user-key": "490204d27c988ccb9e991f177de168ad"
+                }
+            });
+    }
 
+    function geoLoc(response) {
+        console.log(response);
+    }
 
-
-    $.ajax(requestURL + "?api-key=" + ApiKey + "&lat=" + lat + "&lon=" + lon,
-        {
-            method: "GET",
-            success: geoLoc,
-        }
-
-
-
-    // // /Example
-    // var settings = {
-    //   async: true,
-    //   crossDomain: true,
-    //   url: "https://ip-geo-location.p.rapidapi.com/ip/37.140.128.11?format=json",
-    //   method: "GET",
-    //   headers: {
-    //     "x-rapidapi-host": "ip-geo-location.p.rapidapi.com",
-    //     "x-rapidapi-key": "ca48c06477mshca75f4e1c6d9d69p1aaabbjsn3192200da84b"
-    //   }
-    // };
-
-    // // $.ajax(settings).done(function (response) {
-    // //   $("#results").text(response.city.name);
-
-    // //   console.log(response);
 });
