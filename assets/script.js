f9b354e3c9d2ca0142ac7920d92a9817005e2469
Michael Myers <anthonymyers1985@gmail.com>
2020-02-11 20:47:45 -0800

    function on click of more or less than 25.00

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,41 @@
-
-$(document).ready(function () {
+$(document).ready(function() {
     var ApiKey = "490204d27c988ccb9e991f177de168ad";
     var requestURL = "https://developers.zomato.com/api/v2.1/geocode";
 
 
+    $('#money-btn').click(function() {
 
-    if ("geolocation" in navigator) {
+        var money = $('#money').val();
 
-        navigator.geolocation.getCurrentPosition(function (position) {
+        if (money <= '25.00') {
 
-            x = position.coords.latitude;
-            y = position.coords.longitude;
-            ajaxCall(x, y);
-        });
-    } else {
-        console.log("doesn't work")
-    }
 
-    function ajaxCall(x, y) {
+            if ("geolocation" in navigator) {
 
-        $.ajax(requestURL + "?lat=" + x + "&lon=" + y,
-            {
-                type: "GET",
-                success: geoLoc,
-                headers: {
-                    "user-key": "490204d27c988ccb9e991f177de168ad"
-                }
-            });
-    }
+                navigator.geolocation.getCurrentPosition(function(position) {
 
-    function geoLoc(response) {
-        console.log(response);
-    }
+                    x = position.coords.latitude;
+                    y = position.coords.longitude;
+                    ajaxCall(x, y);
+                });
+            } else {
+                console.log("doesn't work")
+            }
 
+            function ajaxCall(x, y) {
+
+                $.ajax(requestURL + "?lat=" + x + "&lon=" + y, {
+                    type: "GET",
+                    success: geoLoc,
+                    headers: {
+                        "user-key": "490204d27c988ccb9e991f177de168ad"
+                    }
+                });
+            }
+
+            function geoLoc(response) {
+                console.log(response);
+            }
+        }
+    })
 });
