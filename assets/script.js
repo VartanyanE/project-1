c68dea9874ca8f59ee6d04ff26f2f432403c8bed
Michael Myers <anthonymyers1985@gmail.com>
2020-02-12 18:48:13 -0800

    Modified Money JS, added Modal and More CSS

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,68 @@
-$(document).ready(function () {
+$(document).ready(function() {
     var ApiKey = "490204d27c988ccb9e991f177de168ad";
     var requestURL = "https://developers.zomato.com/api/v2.1/geocode";
 
 
-    $('#money-btn').click(function () {
+    $('#money-btn').click(function() {
 
         var money = $('#money').val();
 
         if (money <= '25.00') {
 
+            if (money <= 25.00) {
 
-            if ("geolocation" in navigator) {
+                if (money > 26.00, 26) {
 
-                navigator.geolocation.getCurrentPosition(function (position) {
+                    var modal = document.getElementById("myModal");
 
-                    x = position.coords.latitude;
-                    y = position.coords.longitude;
-                    ajaxCall(x, y);
-                });
-            } else {
-                console.log("doesn't work")
-            }
+                    var btn = document.getElementById('money-btn');
 
-            function ajaxCall(x, y) {
+                    var span = document.getElementsByClassName("close")[0];
 
-                $.ajax(requestURL + "?lat=" + x + "&lon=" + y, {
-                    type: "GET",
-                    success: geoLoc,
-                    headers: {
-                        "user-key": "490204d27c988ccb9e991f177de168ad"
+                    btn.onclick = function() {
+                        modal.style.display = "block";
                     }
-                });
-            }
 
-            function geoLoc(data) {
-                data.nearby_restaurants.forEach(function (x) {
-                    var name = x.restaurant.name;
-                    console.log(name);
-                })
+                    span.onclick = function() {
+                        modal.style.display = "none";
+                    }
+
+                    window.onclick = function(event) {
+                        if (event.target == modal) {
+                            modal.style.display = "none";
+                        }
+                    }
+                }
+
+                if ("geolocation" in navigator) {
+
+                    navigator.geolocation.getCurrentPosition(function(position) {
+
+                        x = position.coords.latitude;
+                        y = position.coords.longitude;
+                        ajaxCall(x, y);
+                    });
+                } else {
+                    console.log("doesn't work")
+                }
+
+                function ajaxCall(x, y) {
+
+                    $.ajax(requestURL + "?lat=" + x + "&lon=" + y, {
+                        type: "GET",
+                        success: geoLoc,
+                        headers: {
+                            "user-key": "490204d27c988ccb9e991f177de168ad"
+                        }
+                    });
+                }
+
+                function geoLoc(data) {
+                    data.nearby_restaurants.forEach(function(x) {
+                        var name = x.restaurant.name;
+                        console.log(name);
+                    })
+                }
             }
         }
     })
